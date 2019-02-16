1aa60026cf63a423f099e152ca03b1d2d253cb61
dmanikt <dmanikt@gmail.com>
2019-02-16 18:45:16 -0500

    add field-oriented drive test

---
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/tests/FOD.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.team9450.tests;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.team9450.sensors.Gyroscope;
+import org.firstinspires.ftc.team9450.subsystems.Drivetrain;
+import org.firstinspires.ftc.team9450.util.Vector2D;
+
+public class FOD extends OpMode {
+    private Drivetrain drive;
+    private Gyroscope imu;
+
+    @Override
+    public void init() {
+        drive = new Drivetrain(hardwareMap);
+        imu = new Gyroscope(hardwareMap.get(BNO055IMU.class, "imu"));
+    }
+
+    @Override
+    public void loop() {
+        Vector2D v = new Vector2D();
+        v.x = gamepad1.left_stick_x + (gamepad1.dpad_left? -0.5: gamepad1.dpad_right? 0.5:0);
+        v.y = -gamepad1.left_stick_y + (gamepad1.dpad_down? -0.5: gamepad1.dpad_up? 0.5:0);
+        v.rotate(imu.getAngle()); // TODO: add 45
+        float z = gamepad1.right_stick_x + (gamepad1.right_trigger - gamepad1.left_trigger)/2;
+        double[] driveSignal = new double[]{0,0,0,0};
+        driveSignal[0]= v.x + v.y + z; // up on left stick is -1.
+        driveSignal[1]= -v.x + v.y + z;
+        driveSignal[2]= -v.x + v.y - z;
+        driveSignal[3]= v.x + v.y - z;
+        drive.setPower(driveSignal);
+    }
+}
