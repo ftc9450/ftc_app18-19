d500b8ec4cba292967b019eab94fb9c9b0956d64
dmanikt <dmanikt@gmail.com>
2019-02-27 09:25:07 -0500

    new gaussian for easing

--- Team9450/src/main/java/org/firstinspires/ftc/team9450/tests/auto/StraightPath.java
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/tests/auto/StraightPath.java
@@ -26,7 +26,7 @@ public class StraightPath extends LinearOpMode {
         double correction = imu.getAngle()/100;
         double power;
         double OFFSET;
-        //Gaussian gauss = Gaussian(1.5, );
+        Gaussian gauss = new Gaussian(TARGET/2.0, TARGET/6.0);
 
         waitForStart();
         while (opModeIsActive() && error > 0) {
@@ -35,7 +35,8 @@ public class StraightPath extends LinearOpMode {
             OFFSET = 0.3; //TODO: @Tangerine tweak this value to get robot to start moving
             //power = 0.6*2.0/(0.5*Math.sqrt(2*Math.PI)) * Math.exp(-((error*3.0/TARGET)-1.5)*((error*3.0/TARGET)-1.5)/(2*0.25)) + OFFSET;
             //power = 0.3;//0.7 / (1.0 + Math.exp(-error/(TARGET) + 4));
-            power = ((100000/(curvewidth*Math.sqrt(2*Math.PI))) * Math.exp(-0.5*error-(TARGET/1)/curvewidth))+0.1;
+            //power = ((100000/(curvewidth*Math.sqrt(2*Math.PI))) * Math.exp(-0.5*error-(TARGET/1)/curvewidth))+0.1;
+            power = gauss.value(error) + 0.05;
             correction = imu.getAngle()/100;
             drive.setPower(new double[]{power + correction, power + correction, power - correction, power - correction});
             telemetry.addData("power", power);
