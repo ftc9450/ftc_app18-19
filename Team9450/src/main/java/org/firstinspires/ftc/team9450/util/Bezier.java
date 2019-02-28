202b35b578049cc621dd2919611fd10c2e229687
Vincent <thederpityderp@gmail.com>
2019-02-28 17:37:52 -0500

    Added length (approximation) function, and removed the UnivariateFunction integration stuff

--- Team9450/src/main/java/org/firstinspires/ftc/team9450/util/Bezier.java
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/util/Bezier.java
@@ -1,13 +1,12 @@
 package org.firstinspires.ftc.team9450.util;
 
-import org.apache.commons.math3.analysis.UnivariateFunction;
-import org.apache.commons.math3.analysis.integration.IterativeLegendreGaussIntegrator;
-
-public class Bezier implements UnivariateFunction {
+public class Bezier {
     private double[] p1;
     private double[] p2;
     private double[] a1;
     private double[] a2;
+
+    final double RESOLUTION = 1000.0;
 
     public Bezier(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
         p1 = new double[]{x1, y1};
@@ -16,36 +15,29 @@ public class Bezier implements UnivariateFunction {
         a2 = new double[]{x4, y4};
     }
 
-    private static class BezierX implements UnivariateFunction {
-        @Override
-        public double value(double x) {
-            return this.value(x);
-        }
+    public double valueX(double t) {
+        return Math.pow(1.0 - t, 3)*p1[0] + 3*Math.pow(1.0 - t, 2)*t*(a1[0] + a2[0]) + Math.pow(t, 3)*p2[0];
     }
 
-    private static class BezierY implements UnivariateFunction {
-
-        @Override
-        public double value(double x) {
-            return this.value(x);
-        }
+    public double valueY(double t) {
+        return Math.pow(1.0 - t, 3)*p1[1] + 3*Math.pow(1.0 - t, 2)*t*(a1[1] + a2[1]) + Math.pow(t, 3)*p2[1];
     }
 
-    public double value(double t) {
-        double x = Math.pow(1.0 - t, 3)*p1[0] + 3*Math.pow(1.0 - t, 2)*t*(a1[0] + a2[0]) + Math.pow(t, 3)*p2[0];
-        double y = Math.pow(1.0 - t, 3)*p1[1] + 3*Math.pow(1.0 - t, 2)*t*(a1[1] + a2[1]) + Math.pow(t, 3)*p2[1];
-        return x;
+    public double derivativeX(double t){
+        return 3*Math.pow(1.0 - t, 2)*p1[0] + 6*Math.pow(1.0 - t, 2)*t*(a1[0] + a2[0]) + 3*Math.pow(1.0 - t, 3)*(a1[0] + a2[0]) + 3*Math.pow(t, 2)*p2[0];
     }
 
-    public double[] derivative(double t) {
-        double x = 3*Math.pow(1.0 - t, 2)*p1[0] + 6*Math.pow(1.0 - t, 2)*t*(a1[0] + a2[0]) + 3*Math.pow(1.0 - t, 3)*(a1[0] + a2[0]) + 3*Math.pow(t, 2)*p2[0];
-        double y = 3*Math.pow(1.0 - t, 2)*p1[1] + 6*Math.pow(1.0 - t, 2)*t*(a1[1] + a2[1]) + 3*Math.pow(1.0 - t, 3)*(a1[1] + a2[1]) + 3*Math.pow(t, 2)*p2[1];
-        return new double[]{x, y};
+    public double derivativeY(double t){
+        return 3*Math.pow(1.0 - t, 2)*p1[1] + 6*Math.pow(1.0 - t, 2)*t*(a1[1] + a2[1]) + 3*Math.pow(1.0 - t, 3)*(a1[1] + a2[1]) + 3*Math.pow(t, 2)*p2[1];
     }
 
     public double length() {
-        IterativeLegendreGaussIntegrator legume = new IterativeLegendreGaussIntegrator(10, 1, 1);
-        legume.integrate(1, this, 1, 1);
-        return 2000;
+        double result = 0.0;
+        for(double i = 0; i < RESOLUTION; i++){
+            double dx = valueX((i + 1)/RESOLUTION) - valueX(i/RESOLUTION);
+            double dy = valueY((i + 1)/RESOLUTION) - valueY(i/RESOLUTION);
+            result += Math.sqrt(dx * dx + dy * dy);
+        }
+        return result;
     }
 }
