92c568c0b92cb535880fa326f8819accff1e8a44
Avi Trost <avitrost@gmail.com>
2019-02-28 20:15:35 -0500

    pid tests added

--- Team9450/src/main/java/org/firstinspires/ftc/team9450/tests/subsystems/IntakePID.java
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/tests/subsystems/IntakePID.java
@@ -5,10 +5,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
-public class ElevatorPID extends LinearOpMode {
+public class IntakePID extends LinearOpMode {
 
     // our DC motor.
-    DcMotorEx elevator;
+    DcMotorEx intake;
 
     public static final double NEW_P = 2.5;
     public static final double NEW_I = 0.1;
@@ -18,32 +18,32 @@ public class ElevatorPID extends LinearOpMode {
         // get reference to DC motor.
         // since we are using the Expansion Hub,
         // cast this motor to a DcMotorEx object.
-        elevator = (DcMotorEx)hardwareMap.get(DcMotor.class, "lifter");
+        intake = (DcMotorEx)hardwareMap.get(DcMotor.class, "slider");
 
         // wait for start command.
         waitForStart();
 
         // get the PID coefficients for the RUN_USING_ENCODER  modes.
-        PIDFCoefficients pidOrig = elevator.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+        PIDFCoefficients pidOrig = intake.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // change coefficients using methods included with DcMotorEx class.
         PIDFCoefficients pidNew = new PIDFCoefficients(NEW_P, NEW_I, NEW_D, 0);
-        elevator.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidNew);
+        intake.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidNew);
 
         // re-read coefficients and verify change.
-        PIDFCoefficients pidModified = elevator.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+        PIDFCoefficients pidModified = intake.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // display info to user.
         while(opModeIsActive()) {
             if(gamepad1.x){
-                elevator.setTargetPosition(1000); //TODO: check
-                elevator.setPower(0.3);
+                intake.setTargetPosition(1000); //TODO: check
+                intake.setPower(0.3);
             }else if(gamepad1.y){
-                elevator.setTargetPosition(200); //TODO: check
-                elevator.setPower(0.3);
+                intake.setTargetPosition(200); //TODO: check
+                intake.setPower(0.3);
             }
 
-            telemetry.addData("Position", elevator.getCurrentPosition());
+            telemetry.addData("Position", intake.getCurrentPosition());
             telemetry.addData("Runtime", "%.03f", getRuntime());
             telemetry.addData("P,I,D (orig)", "%.04f, %.04f, %.0f",
                     pidOrig.p, pidOrig.i, pidOrig.d);
