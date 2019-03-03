7997918806e0983ef9bb245bd25045c1f9c7eb21
Vincent <thederpityderp@gmail.com>
2019-03-02 20:37:33 -0500

    Added a PID tuning helper thingy

---
+++ Team9450/src/main/java/org/firstinspires/ftc/team9450/tests/subsystems/IntakePIDTuning.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.team9450.tests.subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+@Autonomous
+public class IntakePIDTuning extends LinearOpMode {
+
+    // our DC motor.
+    DcMotorEx intake;
+
+    public static double NEW_P = 2.5;
+    public static double NEW_I = 0;
+    public static double NEW_D = 0;
+
+    public static final double increment = 0.001;
+
+    public void runOpMode() {
+        // get reference to DC motor.
+        // since we are using the Expansion Hub,
+        // cast this motor to a DcMotorEx object.
+        intake = (DcMotorEx)hardwareMap.get(DcMotor.class, "slider");
+
+        // wait for start command.
+        waitForStart();
+
+        // get the PID coefficients for the RUN_USING_ENCODER  modes.
+        PIDFCoefficients pidOrig = intake.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // change coefficients using methods included with DcMotorEx class.
+        PIDFCoefficients pidNew = new PIDFCoefficients(NEW_P, NEW_I, NEW_D, 0);
+        intake.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidNew);
+
+        // re-read coefficients and verify change.
+        PIDFCoefficients pidModified = intake.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // display info to user.
+        while(opModeIsActive()) {
+            if(gamepad1.x){
+                intake.setTargetPosition(1000); //TODO: check
+                intake.setPower(0.3);
+            }else if(gamepad1.y){
+                intake.setTargetPosition(200); //TODO: check
+                intake.setPower(0.3);
+            }
+
+            if(gamepad1.dpad_up){
+                NEW_P += increment;
+                pidModified = new PIDFCoefficients(NEW_P, NEW_I, NEW_D, 0);
+                intake.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidModified);
+            } else if(gamepad1.dpad_up){
+                NEW_P -= increment;
+                pidModified = new PIDFCoefficients(NEW_P, NEW_I, NEW_D, 0);
+                intake.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidModified);
+            }
+
+            telemetry.addData("Position", intake.getCurrentPosition());
+            telemetry.addData("Runtime", "%.03f", getRuntime());
+            telemetry.addData("P,I,D (orig)", "%.04f, %.04f, %.0f",
+                    pidOrig.p, pidOrig.i, pidOrig.d);
+            telemetry.addData("P,I,D (modified)", "%.04f, %.04f, %.04f",
+                    pidModified.p, pidModified.i, pidModified.d);
+            telemetry.update();
+        }
+    }
+}
